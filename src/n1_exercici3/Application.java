a55cd3aea1c2f6864e6baf04575af1aadc267681
Dario <dariomsn@yahoo.com.ar>
2022-11-20 11:49:15 -0300

    Finalizado

---
+++ src/n1_exercici3/Application.java
@@ -0,0 +1,14 @@
+package n1_exercici3;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		JuegoCapitales2 jc = new JuegoCapitales2();
+		jc.LeerLinea();
+		jc.ListaAleatoria();
+		jc.pedirNombre();
+		jc.jugar();
+	}
+	
+}
